e262ffee48e0a05a12038f49c3e95c65a392960d
yash <yashsavaliya7276@gmail.com>
2023-02-26 15:28:27 +0530

    comitted

---
+++ lib/welcome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Wlcome extends StatefulWidget {
+  const Wlcome({Key? key}) : super(key: key);
+
+  @override
+  State<Wlcome> createState() => _WlcomeState();
+}
+
+class _WlcomeState extends State<Wlcome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
